951e1b767d793d6ef385a2b0def9f950cf79fa27
kennyhw <kennyhermawan7@gmail.com>
2020-02-02 01:39:43 +0800

    Implement initial layouting & styling

--- Gemfile
+++ Gemfile
@@ -17,6 +17,10 @@ gem 'webpacker', '~> 4.0'
 gem 'jbuilder', '~> 2.7'
 # JR Framework
 gem 'jsonapi-resources'
+# Bootstrap for layout and styling
+gem 'bootstrap', '~> 4.4.1'
+# JQuery rails to work with Bootstrap
+gem 'jquery-rails'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 4.0'
 # Use Active Model has_secure_password
--- app/javascript/components/Main.js
+++ app/javascript/components/Main.js
@@ -19,21 +19,43 @@ function Main() {
   }, []);
 
   return (
-    <div>
-      <h1>CheckMark</h1>
-      <hr></hr>
-      <nav>
-        <Link to="/add">New Category</Link>
-      </nav>
-      <ul>
-        {categories.map(category => <li><strong>{category.attributes.title}</strong>{" "}
-                                    <Link to={"category/" + category.id}>View</Link>{" "}
-                                    <Link to={"/edit/" + category.id}>Edit</Link>{" "}
-                                    <Link to={"/delete/" + category.id}>Delete</Link>
-                                    </li>)}
-      </ul>
-      <hr></hr>
-      <Router>
+    <div class="container-fluid">
+      <div class="row card bg-primary text-white h-100 card-body d-flex flex-column align-items-start">
+        <div class="col-md-12 text-center"><h1>CheckMark</h1></div>
+        <div class="col-md-12 text-center"><h4><small>Task Management Application</small></h4></div>
+      </div>
+      <br></br>
+      <div class="row">
+        <div class="col-md-12">
+          <h3 class="featurette"><strong>Categories</strong></h3>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <Link to="/add">New Category</Link>
+        </div>
+      </div>
+      <br></br>
+      <table class="table table-hover">
+        {categories.map(category => 
+          <tr>
+            <td>
+              <div class="row">
+                <div class="col-md-3 navbar-brand">
+                  <strong>{category.attributes.title}</strong>
+                </div>
+                <div class="col-md-3 col-md-offset-1">
+                  <Link className="btn btn-primary mr-1" to={"category/" + category.id}>View</Link>{" "}
+                  <Link className="btn btn-primary mr-1" to={"/edit/" + category.id}>Edit</Link>{" "}
+                  <Link className="btn btn-primary mr-1" to={"/delete/" + category.id}>Delete</Link>
+                </div>
+              </div>
+            </td>
+          </tr>
+        )}
+      </table>
+      <hr class="divider"></hr>
+      <Router primary={false}>
         <AddItem path="category/:categoryId/new-item" />
         <ItemIndex path="category/:categoryId" />
         <ViewItem path="category/:categoryId/view-item/:itemId" />
